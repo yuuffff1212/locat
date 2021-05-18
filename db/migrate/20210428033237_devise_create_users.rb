372d8d59ad818ec7f900c6e92fbcd5ffdcb9f426
yuuffff1212 <75609235+yuuffff1212@users.noreply.github.com>
2021-05-18 19:14:12 +0900

    null:falseの設定

--- db/migrate/20210428033237_devise_create_users.rb
+++ db/migrate/20210428033237_devise_create_users.rb
@@ -7,7 +7,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
       t.string :name,               null: false
-      t.text :profile,            null: false
+      t.text :profile, null: false
 
                ## Recoverable
       t.string   :reset_password_token
