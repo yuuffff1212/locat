2bbfb660bae3c35f5408b7622ffb5ea10e0138fb
yuuffff1212 <75609235+yuuffff1212@users.noreply.github.com>
2021-05-20 15:17:51 +0900

    アイコン画像の作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,6 @@ class ApplicationController < ActionController::Base
   private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :profile])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :profile, :avatar])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :uploads
-  has_one_attached :image
+  has_one_attached :avatar
   with_options presence: true do
     validates :email
     validates :password, format: {with:/\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: "is invalid. Include both letters and numbers. Input half-width characters." }
     validates :name
     validates :profile, length: { maximum: 300}
+    validates :avatar
   end
 end
