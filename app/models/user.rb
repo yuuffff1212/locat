ce4e46468d0222667599a53e22712649138f65e4
yuuffff1212 <75609235+yuuffff1212@users.noreply.github.com>
2021-05-27 17:08:18 +0900

    プロフィールと登録情報の編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,11 @@ class User < ApplicationRecord
   has_one_attached :avatar
   with_options presence: true do
     validates :email
-    validates :password, format: {with:/\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: "is invalid. Include both letters and numbers. Input half-width characters." }
+    validates :password, format: {with:/\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: "is invalid. Include both letters and numbers. Input half-width characters." }, allow_blank: true
     validates :name
     validates :profile, length: { maximum: 300}
     validates :avatar
   end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   root to: "uploads#index"
   resources :uploads
   resources :users, only: [:show, :edit, :update]
+
 end
