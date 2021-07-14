1123a321972fecad8556cb7cbd4a557e389e1bf9
yuuffff1212 <75609235+yuuffff1212@users.noreply.github.com>
2021-07-14 12:24:20 +0900

    Revert "stash"
    This reverts commit d1da3b14675a76d542dee23c90c28c946a20b286.

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -2,19 +2,11 @@ import 'babel-polyfill'
 import Vue from 'vue'
 
 // 作成したコンポーネントファイルをimportします
-<<<<<<< Updated upstream
 import FavoriteButton from '../compenents/Favorite/FavoriteButton.vue'
-=======
-import FavoriteButton from '../components/Favorite/FavoriteButton'
->>>>>>> Stashed changes
 
 document.addEventListener('DOMContentLoaded', () => {
     new Vue({
         el: '#favorite',
         components: { FavoriteButton }
     })
-<<<<<<< Updated upstream
 })
-=======
-})
->>>>>>> Stashed changes
