abe87c79dc1a475a2498b9961a19d722ae868d84
yuuffff1212 <75609235+yuuffff1212@users.noreply.github.com>
2021-07-30 00:27:38 +0900

    entrypoinnt.shの修正

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,9 @@ set -e
 
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /locat/tmp/pids/server.pid
-bundle exec rails db:create
-bundle exec rails db:migrate
-bundle exec rails db:seed
+#bundle exec rails db:create
+#bundle exec rails db:migrate
+#bundle exec rails db:seed
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
