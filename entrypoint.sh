dd0d3c519ea8ecaf358e3cf0af19c62ea450341c
yuuffff1212 <75609235+yuuffff1212@users.noreply.github.com>
2021-08-31 22:16:26 +0900

    環境変数の編集

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 rm -f /locat/tmp/pids/server.pid
 bundle exec rails db:create
 bundle exec rails db:migrate
-bundle exec rails db:seed
+#bundle exec rails db:seed
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
