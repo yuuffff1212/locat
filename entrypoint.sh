42cde7d087d63246ebaf17ae6810690b73e553a1
yuuffff1212 <75609235+yuuffff1212@users.noreply.github.com>
2021-09-17 18:04:41 +0900

    データベースのリセット

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,12 +3,12 @@ set -e
 
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /locat/tmp/pids/server.pid
-bundle exec rails db:create
-bundle exec rails db:migrate
-#bundle exec rails webpacker:compile
-
-bundle exec rails assets:precompile
+bundle exec rails db:drop
+#bundle exec rails db:create
+#bundle exec rails db:migrate
 #bundle exec rails db:seed
+#bundle exec rails webpacker:compile
+bundle exec rails assets:precompile
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
