3f7e783cf6392bd6ad4cb88bcec1393bd3207347
yuuffff1212 <75609235+yuuffff1212@users.noreply.github.com>
2021-09-17 18:38:26 +0900

    update prod.Dockerfile

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,6 @@ rm -f /locat/tmp/pids/server.pid
 #bundle exec rails db:seed
 #bundle exec rails webpacker:compile
 bundle exec rails assets:precompile
-bundle exec rails db:drop RAILS_ENV=production DISABLE_DATABASE_ENVIRONMENT_CHECK=1
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
