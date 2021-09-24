208053825f17185f48e37721b044a7661bf34bf4
yuuffff1212 <75609235+yuuffff1212@users.noreply.github.com>
2021-09-25 02:03:23 +0900

    update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,8 +8,8 @@ rm -f /locat/tmp/pids/server.pid
 #bundle exec rails db:migrate
 #bundle exec rails db:seed
 #bundle exec rails webpacker:compile
-#bundle exec rails assets:precompile
-bundle exec rake assets:precompile
+bundle exec rails assets:precompile
+#bundle exec rake assets:precompile
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
